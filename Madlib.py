1bc48d2afc789a6a1a1330080b60ad12b7b963ef
CD1904 <cduffyhayes19@gmail.com>
2025-04-06 02:08:40 +0100

    Create Madlib.py

---
+++ Madlib.py
@@ -0,0 +1,12 @@
+adjective1 = input("Enter an adjective: ")
+noun = input("Enter a noun: ")
+adjective2 = input("Enter an adjective: ")
+verb = input("Enter a verb: ")
+adjective3 = input("Enter an adjective: ")
+
+
+
+print(f"Today i went to {adjective1} zoo.")
+print(f"In an exhibit, i saw {noun}")
+print(f"{noun} was {adjective2} and {verb}ing")
+print(f"I was {adjective3}")
